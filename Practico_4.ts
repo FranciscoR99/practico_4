f9aa80cbecf6551b6c1c44a962e076e6ea66c1bd
dantesssss <danttramirez137@gmail.com>
2023-06-29 23:13:13 -0300

    abc

--- Practico_4.ts
+++ Practico_4.ts
@@ -1,26 +1,34 @@
 class Sistema_votos{
-    partidos_politicos:Partidos[]
+    partidos_politicos:partido[];
+}
+Class Personas {
+    nombres: string;
+    apellidos: string;
+    dni: number;
+    direccion: string;
+    f_nac: Date;
+    
+    constructor(nombre:string, apellido:string, dni:number ,direccion:string, f_nac:Date){
+        nombre.this = nombre;
+        apellido.this = apellido;
+        dni.this = dni;
+        direccion.this = direccion;
+        f_nac.this = f_nac;
+    }
 }
 
-class Persona{
-    nombre:string
-    apellido:string
-    dni:number
-    direccion:string
-    f_nac:Date
-    constructor(nombre,apellido,dni,direccion,f_nac){
-        nombre=this.nombre
-        apellido=this.apellido
-        dni=this.dni
-        direccion=this.direccion
-        f_nac=this.f_nac
-    }
-}
-class Candidatos extends Persona{
-    puesto
-    constructor(nombre,apellido,dni,direccion,f_nac){
+class Candidatos extends Personas{
+    partidoPolitico:string
+    constructor(nombre:string,apellido:string,dni:number,direccion:string,f_nac:date){
         super(nombre,apellido,dni,direccion,f_nac)
     }
+}
+class Partido{    
+    nombre: string;
+    afiliados: string;
+    constructor(nombre:string,afiliados:string){
+        super(partidoPolitico)
+    
 }
 class Partidos{}
 class Listas{}
